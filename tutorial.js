f15f4e2b10ca4555ea8d7f696a08b33546592b36
CeciliaSwe <cedergren.cecilia@gmail.com>
2021-07-23 07:18:33 +0000

    Add tutorial js for Q

---
+++ tutorial.js
@@ -0,0 +1,29 @@
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions =  [];
+
+let questions = [
+    {
+        question: ""What is 2+2",
+        choice1: "2",
+        choice2: "4",
+        choice3: "8",
+        answer: 2,
+    },
+    {
+        question: ""What is 3x3",
+        choice1: "9",
+        choice2: "18",
+        choice3: "35",
+        answer: 1,
+    },
+    {
+        question: ""What is 4-4",
+        choice1: "2",
+        choice2: "16",
+        choice3: "0",
+        answer: 3,
+    }
+]
