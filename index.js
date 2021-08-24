e3005eac2ece0b0795c88897a1b0240db4ce9f3e
CeciliaSwe <cedergren.cecilia@gmail.com>
2021-08-24 17:39:54 +0000

    add

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // pos is position of where the user in the test or which question they're up to
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+var pos = 0;
+var correct = 0;
+var test, test_status, question, choice, choices, chA, chB, chC;
+
 // this is a multidimensional array with 4 inner array elements with 5 elements inside them
 var questions = [
   {
@@ -28,60 +31,64 @@ var questions = [
   },
   {
     question: "Which is a Minecraft character?",
-    choice1: "Creeper",
-    choice2: "Crawler",
-    choice3: "Crowbar",
+    a: "Creeper",
+    b: "Crawler",
+    c: "Crowbar",
     answer: 1,
     category: "Minecraft",
   },
   {
     question: "Who created Minecraft?",
-    choice1: "Anders Jansson",
-    choice2: "Johan Larsson",
-    choice3: "Markus Persson",
+    a: "Anders Jansson",
+    b: "Johan Larsson",
+    c: "Markus Persson",
     answer: 3,
     category: "Minecraft",
   },
   {
     question: "What shape are building blocks?",
-    choice1: "square",
-    choice2: "circular",
-    choice3: "hexagon",
+    a: "square",
+    b: "circular",
+    c: "hexagon",
     answer: 1,
     category: "Minecraft",
   },
   {
       question: "What is 3x3",
-      choice1: "9",
-      choice2: "18",
-      choice3: "35",
+      a: "9",
+      b: "18",
+      c: "35",
       answer: 1,
       category: "Maths",
   },
   {
     question: "What is 24 - 8",
-    choice1: "16",
-    choice2: "13",
-    choice3: "19",
+    a: "16",
+    b: "13",
+    c: "19",
     answer: 1,
     category: "Maths",
   },
   {
       question: "What is 4-4",
-      choice1: "2",
-      choice2: "16",
-      choice3: "0",
+      a: "2",
+      b: "16",
+      c: "0",
       answer: 3,
       category: "Maths",
   }
 ];
+// shortened version of getElementById function  
+function get(x){
+  return document.getElementById(x);
+}
+
 // this get function is short for the getElementById function  
 function get(x){
   return document.getElementById(x);
 }
 // this function renders a question for display on the page
 function renderQuestion(){
-  shuffle(questions);
   test = get("test");
   if(pos >= questions.length){
     test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
@@ -92,7 +99,7 @@ function renderQuestion(){
     // stops rest of renderQuestion function running when test is completed
     return false;
   }
-  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+  get("test_status").innerHTML = "Question "+(pos+1);
   
   question = questions[pos].question;
   chA = questions[pos].a;
@@ -107,25 +114,6 @@ function renderQuestion(){
   test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
   test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
 }
-
-function shuffle(questions) {
-  var currentIndex = questions.length,  randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [questions[currentIndex], questions[randomIndex]] = [
-      questions[randomIndex], questions[currentIndex]];
-  }
-
-  return questions;
-}
-
 function checkAnswer(){
   // use getElementsByName because we have an array which it will loop through
   choices = document.getElementsByName("choices");
