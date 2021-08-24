ea66083e2bef2e1695d1c275ba7599c6790301ba
CeciliaSwe <cedergren.cecilia@gmail.com>
2021-08-24 11:36:59 +0000

    Add

--- index.js
+++ index.js
@@ -1,108 +1,28 @@
-const quizContainer = document.getElementById("q-out");
-const resultsContainer = document.getElementById("r-out");
-const optionText1 = document.getElementById("option-1");
-const optionText2 = document.getElementById("option-2");
-const optionText3 = document.getElementById("option-3");
-let correctAnswer = "";
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions =  [];
-
-function clickQuestion (event) {
-    
-    let selectDiv = document.getElementsByClassName('category');
-    let classLength = selectDiv.length;
-    /*alert("There are "+classLength+" category classes in the html code");*/
-    let randomCategory = Math.floor(Math.random() * selectDiv.length);
-    console.log(randomCategory, selectDiv[randomCategory]);
-    console.log(randomCategory);
-    /*console.log('Received the ' + event.type + " event!");
-    console.log('"this" currently refers to', this.id);
-    console.log('You clicked the button!\n');*/
-
-    questionValue();
-    lightCategory(randomCategory);   
-    shuffle(quizCatA);
-    shuffle(quizCatB);
-    shuffle(quizCatC);
-    displayFirstQ(randomCategory);
-    
-}
-  
-let questionButton = document.getElementById('question');
-questionButton.addEventListener('click', clickQuestion); // left clicks
-
-function questionValue() {
-    let values = [10, 20, 50, 100];
-    let randomValue = Math.floor(Math.random() * values.length);
-    console.log(values[randomValue]);
-    /*alert(`The value of the question is ${values[randomValue]}.`);*/
-    document.getElementById("value").innerHTML = values[randomValue];
-}
-
-function lightCategory(randomCategory) {
-    if (randomCategory === 0) {
-        /*alert("Picked 1")*/
-        document.getElementById("c-1").style.backgroundColor = "orange";
-    } else if (randomCategory === 1) {
-        /*alert("Picked 2");*/
-        document.getElementById("c-2").style.backgroundColor = "orange";
-    } else if (randomCategory === 2) {
-        /*alert("Picked 3");*/
-        document.getElementById("c-3").style.backgroundColor = "orange";
-    } else if (randomCategory === 3) {
-        /*alert("Picked 4");*/
-        document.getElementById("c-4").style.backgroundColor = "orange";
-    };
-    
-}
-
-
-//Shuffles array according to Fisher-Yates https://bost.ocks.org/mike/shuffle/
-
-function shuffle(quiz) {
-  var currentIndex = quiz.length,  randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [quiz[currentIndex], quiz[randomIndex]] = [
-      quiz[randomIndex], quiz[currentIndex]];
-  }
-
-  return quiz;
-}
-
-// Questions
-
-let quiz = [
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
   {
       question: "How many starter Pokemons can you choose from?",
-      choice1: "1",
-      choice2: "3",
-      choice3: "5",
+      a: "1",
+      b: "3",
+      c: "5",
       answer: 2,
       category: "Pokemon",
   },
   {
     question: "What color is Charmander?",
-    choice1: "Green",
-    choice2: "Blue",
-    choice3: "Orange",
+    a: "Green",
+    b: "Blue",
+    c: "Orange",
     answer: 3,
     category: "Pokemon",
   },
   {
     question: "Which is not a legendary Pokemon?",
-    choice1: "Heatran",
-    choice2: "Dragonite",
-    choice3: "Lugia",
+    a: "Heatran",
+    b: "Dragonite",
+    c: "Lugia",
     answer: 2,
     category: "Pokemon",
   },
@@ -155,64 +75,74 @@ let quiz = [
       category: "Maths",
   }
 ];
-
-//Filter out questions from a specific category
-const quizCatA = quiz.filter(quiz => quiz.category === "Pokemon");
-const quizCatB = quiz.filter(quiz => quiz.category === "Minecraft");
-const quizCatC = quiz.filter(quiz => quiz.category === "Maths");
-
-function displayFirstQ(randomCategory) {
-  if (randomCategory === 0) {
-      /*alert("Picked Pokemon");*/
-      console.log(quizCatA[0]);
-      quizContainer.innerText = quizCatA[0].question;
-      optionText1.innerText = quizCatA[0].choice1;
-      optionText2.innerText = quizCatA[0].choice2;
-      optionText3.innerText = quizCatA[0].choice3;
-      correctAnswer = (quizCatA[0].answer);
-  } else if (randomCategory === 1) {
-      /*alert("Picked Minecraft");*/
-      console.log(quizCatB[0]);
-      quizContainer.innerText = quizCatB[0].question;
-      optionText1.innerText = quizCatB[0].choice1;
-      optionText2.innerText = quizCatB[0].choice2;
-      optionText3.innerText = quizCatB[0].choice3;
-      correctAnswer = (quizCatB[0].answer);
-  } else if (randomCategory === 2) {
-      /*alert("Picked Maths");*/
-      console.log(quizCatC[0]);
-      quizContainer.innerText = quizCatC[0].question;
-      optionText1.innerText = quizCatC[0].choice1;
-      optionText2.innerText = quizCatC[0].choice2;
-      optionText3.innerText = quizCatC[0].choice3;
-      correctAnswer = (quizCatC[0].answer);
-  } else {
-      /*alert("Not defined yet");*/
+// this get function is short for the getElementById function  
+function get(x){
+  return document.getElementById(x);
+}
+// this function renders a question for display on the page
+function renderQuestion(){
+  shuffle(questions);
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+    get("test_status").innerHTML = "Test completed";
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
   }
-};  
-
-// compares chosen answer with correct answer and returns alert
-function checkAnswer(event) {
-  //get the value of the checked radiobutton to compare to correct answer
-  let currentAnswer = document.querySelector('input[name="test"]:checked').value;
-  let currentScore = 0;
-  let addedScore = document.getElementById("value").innerHTML;
-
-  if (parseInt(currentAnswer) === parseInt(correctAnswer)) {
-    alert("WOOP you did it");
-    resultsContainer.innerText = parseInt(currentScore) + parseInt(addedScore);
-    
+  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
   
-    } else{
-      alert("NAH")
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+  // display the question
+  test.innerHTML = "<h3>"+question+"</h3>";
+  // display the answer options
+  // the += appends to the data we started on the line above
+  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+
+function shuffle(questions) {
+  var currentIndex = questions.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [questions[currentIndex], questions[randomIndex]] = [
+      questions[randomIndex], questions[currentIndex]];
+  }
+
+  return questions;
+}
+
+function checkAnswer(){
+  // use getElementsByName because we have an array which it will loop through
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
     }
-    
-    //display in results container
-    
- }
-
-
- let submitAnswer = document.getElementById("submit-answer");
- submitAnswer.addEventListener('click', checkAnswer); // left clicks*/
-
-//all OK until here!
+  }
+  // checks if answer matches the correct choice
+  if(choice == questions[pos].answer){
+    //each time there is a correct answer this value increases
+    correct++;
+  }
+  // changes position of which character user is on
+  pos++;
+  // then the renderQuestion function runs again to go to next question
+  renderQuestion();
+}
+// Add event listener to call renderQuestion on page load event
+window.addEventListener("load", renderQuestion);
--- questions.js
+++ questions.js
@@ -1,116 +1,75 @@
 
-    function buildQuiz(){
-      // variable to store the HTML output
-      const outputQ = [];
-      const outputA = [];
-  
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          outputQ.push(
-            `<div class="question"> ${currentQuestion.question} </div>`
-          );
-          outputA.push(
-            `<div class="answers"> ${answers.join('')} </div>`
-          );
-        }
-      );
-  
-      // display Q and A in separate containers
-      quizContainer.innerHTML = outputQ;
-      optionContainer.innerHTML = outputA;
-    }
-  
-    function showResults(){
-  
-      // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
-        }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-  
-    const quizContainer = document.getElementById("q-out");
-    const resultsContainer = document.getElementById("r-out");
-    const submitButton = document.getElementById('submit');
-    const optionContainer = document.getElementById("o-out")
-    const myQuestions = [
-      {
-        question: "What is the name of the yellow starter Pokemón?",
-        answers: {
-          a: "Charmander",
-          b: "Bulbasaur",
-          c: "Pikachu"
+var questions = [
+        {
+            question: "How many starter Pokemons can you choose from?",
+            a: "1",
+            b: "3",
+            c: "5",
+            answer: 2,
+            category: "Pokemon",
         },
-        correctAnswer: "c"
-      },
-      {
-        question: "Which one of these is a JavaScript package manager?",
-        answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm"
+        {
+          question: "What color is Charmander?",
+          a: "Green",
+          b: "Blue",
+          c: "Orange",
+          answer: 3,
+          category: "Pokemon",
         },
-        correctAnswer: "c"
-      },
-      {
-        question: "Which tool can you use to ensure code quality?",
-        answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
+        {
+          question: "Which is not a legendary Pokemon?",
+          a: "Heatran",
+          b: "Dragonite",
+          c: "Lugia",
+          answer: 2,
+          category: "Pokemon",
         },
-        correctAnswer: "d"
-      }
-    ];
-  
-    // Kick things off
-    buildQuiz();
-  
-    // Event listeners
-    submitButton.addEventListener('click', showResults);
+        {
+          question: "Which is a Minecraft character?",
+          choice1: "Creeper",
+          choice2: "Crawler",
+          choice3: "Crowbar",
+          answer: 1,
+          category: "Minecraft",
+        },
+        {
+          question: "Who created Minecraft?",
+          choice1: "Anders Jansson",
+          choice2: "Johan Larsson",
+          choice3: "Markus Persson",
+          answer: 3,
+          category: "Minecraft",
+        },
+        {
+          question: "What shape are building blocks?",
+          choice1: "square",
+          choice2: "circular",
+          choice3: "hexagon",
+          answer: 1,
+          category: "Minecraft",
+        },
+        {
+            question: "What is 3x3",
+            choice1: "9",
+            choice2: "18",
+            choice3: "35",
+            answer: 1,
+            category: "Maths",
+        },
+        {
+          question: "What is 24 - 8",
+          choice1: "16",
+          choice2: "13",
+          choice3: "19",
+          answer: 1,
+          category: "Maths",
+        },
+        {
+            question: "What is 4-4",
+            choice1: "2",
+            choice2: "16",
+            choice3: "0",
+            answer: 3,
+            category: "Maths",
+        }
+      ];
