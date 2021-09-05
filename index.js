fa8706175f7a404e4eae8255a47fafcd4e7c9dd6
CeciliaSwe <cedergren.cecilia@gmail.com>
2021-09-05 16:05:42 +0000

    update

--- index.js
+++ index.js
@@ -201,11 +201,11 @@ function checkAnswer(event) {
   //get the value of the checked radiobutton to compare to correct answer
   let currentAnswer = document.querySelector('input[name="test"]:checked').value;
   let currentScore = parseInt(resultsContainer.innerHTML);
-  let addedScore = document.getElementById("value").innerHTML;
+  let addedScore = parseInt(document.getElementById("value").innerHTML);
 
   if (parseInt(currentAnswer) === parseInt(correctAnswer)) {
     alert("WOOP you did it");
-    resultsContainer.innerText = currentScore + parseInt(addedScore);
+    resultsContainer.innerText = currentScore + addedScore;
     
   
     } else{
